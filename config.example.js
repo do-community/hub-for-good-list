bc16fd5c2652bf79e8d562ec29c90905f07e4549
MattIPv4 <me@mattcowley.co.uk>
2020-04-29 16:34:02 +0100

    Get the data from Google

---
+++ config.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+    clientEmail: '', // Google account email (OR set with env GOOGLE_CLIENT_EMAIL)
+    privateKey: '', // Google API key (OR set with env GOOGLE_PRIVATE_KEY)
+    sheetId: '', // Google sheet id to use (OR set with env GOOGLE_SHEET_ID)
+};
---
+++ src/build/get.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+const main = async () => {
+    // Load our config from env or file
+    let clientEmail = process.env.GOOGLE_CLIENT_EMAIL;
+    let privateKey = process.env.GOOGLE_PRIVATE_KEY;
+    let sheetId = process.env.GOOGLE_SHEET_ID;
+    if (fs.existsSync(path.join(__dirname, '..', '..', 'config.js'))) {
+        ({ clientEmail, privateKey, sheetId } = require('../../config'));
+    }
+
+    // Connect to the document
+    const doc = new GoogleSpreadsheet(sheetId);
+    await doc.useServiceAccountAuth({
+        client_email: clientEmail,
+        private_key: privateKey,
+    });
+    await doc.loadInfo();
+
+    // Get the approval sheet
+    const sheet = doc.sheetsByIndex[0];
+    const rows = await sheet.getRows();
+
+    // Get the rows that are public & approved
+    const approved = rows.filter(row =>
+        row.is_public && row.status
+        && row.is_public.toLowerCase().trim() === '["yes"]'
+        && row.status.toLowerCase().trim() === 'approved');
+
+    // Only keep the columns we need
+    const sanitised = approved.map(row => ({
+        name: row.project_name || '',
+        link: row.project_link || '',
+        description: row.project_description || '',
+        purpose: row.project_purpose || '',
+        purposeOther: row.project_purpose_other || '',
+    }));
+
+    // Export the data
+    await fs.promises.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(sanitised));
+};
+
+main();
+
+
